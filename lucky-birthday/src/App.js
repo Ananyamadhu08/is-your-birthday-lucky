753835234d864bcb601ad3d1797c15cb46671cd5
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2021-08-18 18:10:13 +0530

    footer, privacy notice and css done

--- lucky-birthday/src/App.js
+++ lucky-birthday/src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
-import{ useState, React} from "react";
+import { useState, React } from "react";
+import happy from "./happy.svg";
+import sad from "./sad.svg";
 
 // Take user's complete birthday
 // And his/her lucky number.
@@ -7,87 +8,159 @@ import{ useState, React} from "react";
 // Show the result to the user.
 
 let dateInput = "";
-let luckyNo= 0;
+let luckyNo = 0;
+
+//var to store images
+
+const happyImageDiv = (
+  <img alt="happyImage" src={happy} width="100%" height="200px" />
+);
+
+const unhappyImageDiv = (
+  <img alt="sadImage" src={sad} width="100%" height="200px" />
+);
 
 function App() {
-  //  state to store the result text and img 
-  const [displayResult, setDisplayResult] = useState(["",""])
-  
-  function checkLuckyBirthday(e){
-    //prevent the refreshing of the page on submit of the form 
-    e.preventDefault()
+  // state to display privacy notice
+  const [displayAlert, setDisplayAlert] = useState("flex");
+  //  state to store the result text and img
+  const [displayResult, setDisplayResult] = useState(["", ""]);
+
+  function checkLuckyBirthday(e) {
+    //prevent the refreshing of the page on submit of the form
+    e.preventDefault();
     // to convert the date into small strings
     const dateArray = dateInput.split("-");
     let sum = 0;
-    // to access each string 
+    // to access each string
     dateArray.map((string) => {
       //to access each number in the string and add into sum
-      for(let i = 0; i < string.length; i++){
+      for (let i = 0; i < string.length; i++) {
         sum = sum + Number(string[i]);
       }
       // exit the map loop with sum or return sum from the map
-      return sum
+      return sum;
+    });
+
+    //now check if the sum is divisible by lucky no
+    if (sum % Number(luckyNo) === 0) {
+      setDisplayResult(["Hurray! You are a lucky person!", happyImageDiv]);
+    } else {
+      setDisplayResult(["Oops! You are unlucky!", unhappyImageDiv]);
     }
-    );
-
-      //now check if the sum is divisible by lucky no
-  if(sum % Number(luckyNo) === 0){
-    setDisplayResult([
-      "Hurray! You are a lucky person!",
-      "well put happ impg here"
-    ])
-  } else {
-    setDisplayResult([
-      "Oops! You are unlucky!",
-      "well put sad img here"]);
   }
-
-  }
-
 
   return (
     <div className="App">
+      {/* header section */}
 
-      <section>
+      <div
+        className="parallax"
+        style={{
+          minHeight: "100vh",
+          backgroundAttachment: "fixed",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="titleOfPage">
+          <h1>Is your birthday lucky?</h1>
+          <a href="#mainSection">👇🏼👇🏼👇🏼👇🏼</a>
+        </div>
+      </div>
+
+      <section id="mainSection" className="section">
+        {/* div for the privacy alert */}
+        <div id="alertBox" style={{ display: `${displayAlert}` }}>
+          <div className="notice">
+            <strong>Privacy Notice!</strong> We are not storing your data
+          </div>
+
+          <div
+            onClick={() => setDisplayAlert("none")}
+            style={{
+              paddingLeft: "1rem",
+              cursor: "pointer",
+              fontSize: "0.5rem",
+            }}
+          >
+            <span role="img"> &#10060; </span>
+          </div>
+        </div>
         <h2>Enter your birthday day and lucky number to continue...</h2>
-        
+
         <form onSubmit={checkLuckyBirthday}>
-          <label className="label" htmlFor="dateInput"/>
-          <input 
-          id="dateInput" 
-          type="date" 
-          required
-          onChange={(e) => {
-            dateInput = e.target.value
-          }}
+          <label className="label" htmlFor="dateInput"></label>
+          <input
+            id="dateInput"
+            type="date"
+            required
+            onChange={(e) => {
+              dateInput = e.target.value;
+            }}
           />
 
-          <label className="label" htmlfor="luckyNumber"/>
+          <label className="label" htmlfor="luckyNumber"></label>
 
-          <input 
-          id="luckyNumber" 
-          type="number"
-          onChange={function xyz(event){
-            luckyNo = event.target.value
-          }}
-          required
+          <input
+            id="luckyNumber"
+            type="number"
+            onChange={function xyz(event) {
+              luckyNo = event.target.value;
+            }}
+            required
           />
 
           <button type="submit">Check</button>
-          
-          <div className="output">
-
-           <div className="label">{displayResult[0]}</div>
-           {displayResult[1]}
-          
-          </div>
-
         </form>
 
         {/* output result div */}
+        <div className="output">
+          <div className="label">{displayResult[0]}</div>
+          {displayResult[1]}
+        </div>
 
+        {/* footer div */}
+
+        <footer>
+          <ul>
+            <li className="footerLink">
+              <a href="https://github.com/Ananyamadhu08">
+                <i className="fab fa-github" aria-hidden="true"></i>
+              </a>
+            </li>
+
+            <li className="footerLink">
+              <a href="">
+                <i className="fab fa-twitter" aria-hidden="true"></i>
+              </a>
+            </li>
+
+            <li className="footerLink">
+              <a href="">
+                <i className="fab fa-linkedin-in" aria-hidden="true"></i>
+              </a>
+            </li>
+
+            <li className="footerLink">
+              <a href="">
+                <i className="fas fa-briefcase" aria-hidden="true"></i>
+              </a>
+            </li>
+          </ul>
+          <div className="legacyText">
+            © 2020 | ananyaMadhu |{" "}
+            <a
+              className="privacy-policy"
+              href="#alertbox"
+              onClick={() => setDisplayAlert("flex")}
+              style={{ cursor: "pointer", color: "black" }}
+            >
+              Privacy Policy
+            </a>
+          </div>
+        </footer>
       </section>
-    
     </div>
   );
 }
